63cf6ad49e72bb787770b8cc09c425e459c70985
Luis Peñaranda <luis.penaranda@gmx.com>
2017-09-17 08:30:50 -0300

    Compute only useful coeffs on exact division.
    In the exact division algorithm, the rest at each iteration is now only
    partially computed.

--- ediv.c
+++ ediv.c
@@ -22,31 +22,31 @@
 #include <gmp.h>
 
 /* Sets s(x)=s(x)-g(x)*k*x^shift. Return the degree of s after subtraction.
+   The result is computed starting in coefficient start, the other
+   coefficients of s are left untouched.
    Preconditions, degs>=degg+shift, shift>=0 and s[degs]!=0. */
-int mug_fms(mpz_t *s,int degs,mpz_t *g,int degg,mpz_ptr k,int shift){
+int mug_fms(mpz_t *s,int degs,mpz_t *g,int degg,mpz_ptr k,int shift,int start){
         size_t i;
-        for(i=0;i<=degg;++i)
-                mpz_submul(s[i+shift],g[i],k);
+        for(i=start;i<=degg+shift;++i)
+                mpz_submul(s[i],g[i-shift],k);
         if(degg+shift==degs)
-                /* Compute the output degree of s and, if needed, clear the
-                   leading zero coefficients of s. */
-                while((degs>0)&&(!mpz_sgn(s[degs]))){
-                        mpz_clear(s[degs]);
+                /* Compute the output degree of s. Note that no leading
+                   zero coefficient of s is cleared, all of them need to
+                   be cleared on exit. */
+                while((degs>0)&&(!mpz_sgn(s[degs])))
                         --degs;
-                }
         return degs;
 }
 
 /* Exact division: sets q(x)=f(x)/g(x). Precondition: the remainder of
    the division is zero. */
 int mug_ediv(mpz_t *q,mpz_t *f,int degf,mpz_t *g,int degg){
-        int i,j,degr;
-        /*mpz_t r,subtracted;*/
+        int i,degr,start;
         mpz_t *r;
         /* Init the coefficients of the quotient q. */
         int allocq=1+degf-degg; /* allocq is one more than the degree of q. */
-        for(j=0;j<allocq;++j)
-                mpz_init(q[j]);
+        for(i=0;i<allocq;++i)
+                mpz_init(q[i]);
         if(!degg){
                 for(i=0;i<=degf;++i)
                         mpz_divexact(q[i],f[i],g[0]);
@@ -54,21 +54,25 @@ int mug_ediv(mpz_t *q,mpz_t *f,int degf,mpz_t *g,int degg){
         }
 
         /* r(x) is the rest of the division on each step, that is, r(x) is
-           initialized as f(x) and at each iteration, r(x)=r(x)-q[i]*g(x). */
-        /* TODO: optimize this, avoiding to compute all the coefficients
-           of r. */
+           initialized as f(x) and at each iteration, r(x)=r(x)-q[i]*g(x).
+           Note that not all the coefficients of r will be computed, we
+           will compute only the coefficients which will be useful in latter
+           iterations. start is the starting useful coefficient of r. */
+        start=degf-degg;
+        /* TODO: allocate less coefficients for r. In practice, we never
+           use coefficients between 0 and start-1. */
         r=(mpz_t*)malloc((degf+1)*sizeof(mpz_t));
-        for(i=0;i<=degf;++i)
+        for(i=start;i<=degf;++i)
                 mpz_init_set(r[i],f[i]);
         degr=degf;
 
         for(i=degf-degg;i>0;--i){
                 mpz_divexact(q[i],r[degr],g[degg]);
-                degr=mug_fms(r,degr,g,degg,q[i],i);
+                degr=mug_fms(r,degr,g,degg,q[i],i,start);
         }
         mpz_divexact(q[0],r[degr],g[degg]);
         /* Free the space allocated for r and its coefficients. */
-        for(i=0;i<=degr;++i)
+        for(i=start;i<=degf;++i)
                 mpz_clear(r[i]);
         free(r);
 
